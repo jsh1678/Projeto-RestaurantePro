6de7a7671b800c014b67a0cc3f47196672f4280a
jsh1678 <jsguilherme81@gmail.com>
2026-05-28 08:13:39 -0300

    Adiciona fotos aos itens do cardapio

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class MenuItem extends Model
+{
+    protected $fillable = [
+        'category_id', 'nome', 'descricao', 'preco', 'disponivel',
+        'stock_item_id', 'serves_count', 'subtipo', 'imagem'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function stockItem(): BelongsTo
+    {
+        return $this->belongsTo(StockItem::class);
+    }
+
+    public function ingredients(): HasMany
+    {
+        return $this->hasMany(MenuItemIngredient::class);
+    }
+}
